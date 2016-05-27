bc666d86cb5db3f305437bb548d3b551f3023e2d
iNTERFACEWARE Inc <admin@interfaceware.com>
2016-05-27 15:19:35 -0400

    Delete getspreadsheet.lua

--- Excel_importer-FromHTTP-msv6YSSFLMTqO9/getspreadsheet.lua
+++
@@ -1,7 +0,0 @@
-local sheet = require 'excel.sheet'
-
-function GetSpreadSheet(R, Auth)
-   sheet.serve{user=Auth.username, sheet='IguanaFeed.xlsm', host=R.headers.Host}
-end
-
-return GetSpreadSheet
